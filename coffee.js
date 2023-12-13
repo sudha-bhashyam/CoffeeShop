4ca40c1702e0f3e64325f3dfd7544030d4924a06
sudhabhashyam <lakshmisudha9090@gmail.com>
2023-12-13 16:01:10 +0530

    first commit

---
+++ coffee.js
@@ -0,0 +1,30 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let  searchForm= document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    cartItem.classList.remove('active');
+    navbar.classList.toggle('active');
+}
+
+let  cartItem= document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+    navbar.classList.toggle('active');
+
+}
